2966d69f66ed05799523876849531ebb74954747
Jasonhan <nianlonghan@gmail.com>
2017-04-17 17:27:16 +0800

    Leet Code algorithm 476 in C++

---
+++ 476_number_complement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findComplement(int num) {
+    	int temp = num;
+        int bitCount = 1;
+        while(temp != 0)
+        {
+        	temp >>= 1;
+        	bitCount <<= 1;
+        }
+        return ((bitCount - 1) ^ num);
+    }
+};
+
+
+/* Following code is referenced from internet
+* int findComplement(int num) {  
+*        unsigned int mask = ~0;  
+*        while(num&mask) mask <<= 1;  
+*        return (~mask) ^ num;  
+*    } 
+*/
