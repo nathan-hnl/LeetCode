5fdfbad71d5b9faffa47021a6a1cff7c7a8e2cdc
Jasonhan <nianlonghan@gmail.com>
2017-04-18 10:53:22 +0800

    Leet Code algorithm 136 in C++

---
+++ 136_single_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int numsCount = nums.size();
+        int result = 0;
+        for (int i = 0; i < numsCount; i++)
+        {
+        	result ^= nums[i];
+        }
+        return result;
+    }
+};
