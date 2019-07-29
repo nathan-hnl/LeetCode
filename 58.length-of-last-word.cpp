b0a2faebcd4976bb344f3d8243296ae19e998c1a
nlhan <nianlonghan@gmail.com>
2019-07-29 15:14:29 +0800

    update solution for problem 58

--- 58.length-of-last-word.cpp
+++ 58.length-of-last-word.cpp
@@ -5,11 +5,15 @@
  */
 
 #include <string>
+//2. using STL need this head file
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        //1. loop method
+        //time complexity O(n), space complexity O(1)
         int len = 0, tail = s.length() - 1;
         while (tail >= 0 && s[tail] == ' ')
         {
@@ -22,5 +26,12 @@ public:
         }
 
         return len;
+
+        //2. using STL
+        // time complexity O(n), space complexity O(1)
+        auto first = find_if(s.rbegin(), s.rend(), ::isalpha);
+        auto last = find_if_not(first, s.rend(), ::isalpha);
+
+        return distance(first, last);
     }
 };
